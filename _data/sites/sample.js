bac3bb440e899859c76533aa2c040605ed6405a9
stardoom4 <139852159+stardoom4@users.noreply.github.com>
2024-02-05 11:51:48 +0530

    Update sample.js

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -1,14 +1,21 @@
 module.exports = {
-	name: "Sample", // optional, falls back to object key
-	description: "The default sites that get tested",
+	name: "celestialentity", // optional, falls back to object key
+	description: "Celestial Entity Personal Website ",
+	// skip if localhost
+	// skip if this is a new fork of the speedlify (not Zach’s)
+	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 	options: {
-		runs: 1,
-		frequency: 1, // (in minutes)
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
 	},
 	urls: [
-		"https://garud.netlify.app/"
-                "https://garud.netlify.app/microblog"
-                "https://garud.netlify.app/articles"
-                "https://garud.netlify.app/shows"
+		"https://garud.netlify.app/",
+		"https://garud.netlify.app/profile/",
+		"https://garud.netlify.app/articles/",
+		"https://garud.netlify.app/microblog/",
 	]
 };
